b08786c8ff614a576811e719df3acb4e926630f3
raimonizard <97727989+raimonizard@users.noreply.github.com>
2023-02-02 00:40:33 +0100

    feat(main menu): create main menu and minor other changes

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 @namespace
 class SpriteKind:
     button = SpriteKind.create()
-    cursor = SpriteKind.create()
 
 def on_b_pressed():
     if settingLevel != 1:
-        mySprite.say_text("Som-hi!!", 500, True)
+        firePlane.say_text("Som-hi!!", 500, True)
 controller.B.on_event(ControllerButtonEvent.PRESSED, on_b_pressed)
+
+# ################################################
+def init_config():
+    if difficulty == 1:
+        game.splash("Bosc petit")
+        tiles.set_tilemap(tilemap("""
+            level1
+        """))
+        game.set_dryness_of_grass(randint(1, 3))
+        game.set_strength_of_wind(randint(1, 3))
+        game.set_health_of_trees(randint(5, 9))
+    elif difficulty == 2:
+        game.splash("Bosc gran")
+        tiles.set_tilemap(tilemap("""
+            level2
+        """))
+        game.set_dryness_of_grass(randint(2, 4))
+        game.set_strength_of_wind(randint(2, 4))
+        game.set_health_of_trees(randint(4, 9))
+    hud.danger_hud_label("Risc d'incendi")
+    hud.danger_hud(True)
+    hud.fire_hud_label("Focs:")
+    hud.fire_hud(True)
+    hud.forest_hud_label("Salut del bosc")
+    hud.forest_hud_healthy(7)
+    hud.forest_hud_burned(2)
+    hud.forest_hud(True)
 
 def on_fire_created(location):
     scene.create_particle_effect_at_location(location, effects.fire)
@@ -14,19 +40,8 @@ def on_fire_created(location):
     music.knock.play()
 sprites.on_fire_created(on_fire_created)
 
-def chooseDifficulty1():
-    global difficulty
-    tiles.set_tilemap(tilemap("""
-        level1
-    """))
-    game.splash("Benvingut/da bomber/a!")
-    difficulty = game.ask_for_number("Escull la dificultat del bosc (1 o 2)", 1)
-    while difficulty != 1 and difficulty != 2:
-        game.splash("Tria 1 o 2")
-        difficulty = game.ask_for_number("Escull dificultat (1 o 2)", 1)
-
 def on_left_pressed():
-    animation.run_image_animation(mySprite,
+    animation.run_image_animation(firePlane,
         assets.animation("""
             Fire Plane 2 Left Animation 1
         """),
@@ -34,55 +49,35 @@ def on_left_pressed():
         True)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
-def on_on_overlap(sprite, otherSprite):
-    global difficulty
-    if settingLevel == 1:
-        if otherSprite == bforestA and controller.A.is_pressed():
-            difficulty = 1
-            chooseDifficulty2()
-        if otherSprite == bforestB and controller.A.is_pressed():
-            difficulty = 1
-            chooseDifficulty2()
-sprites.on_overlap(SpriteKind.cursor, SpriteKind.button, on_on_overlap)
+def start_game():
+    global firePlane
+    firePlane = sprites.create(assets.image("""
+            Fire Plane 2 Right
+        """),
+        SpriteKind.player)
+    firePlane.set_bounce_on_wall(False)
+    controller.move_sprite(firePlane)
+    scene.camera_follow_sprite(firePlane)
+    init_config()
+    music.play(music.string_playable("B G B G B G B G ", 120),
+        music.PlaybackMode.LOOPING_IN_BACKGROUND)
+    game.show_long_text("Prem A per tirar aigua", DialogLayout.TOP)
+    for index in range(randint(4, 10)):
+        sprites.create_spreading_fire(assets.tile("""
+                tree
+            """),
+            assets.tile("""
+                tree fire
+            """))
 
 def on_right_pressed():
-    animation.run_image_animation(mySprite,
+    animation.run_image_animation(firePlane,
         assets.animation("""
             Fire Plane 2 Right Animation
         """),
         300,
         True)
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
-
-def choosePlane():
-    game.splash("hola")
-
-def chooseDifficulty0():
-    global level2
-    level2 = 0
-    game.show_long_text("Escull la dificultat", DialogLayout.FULL)
-    game.show_long_text("Puja o baixa amb les flextes", DialogLayout.BOTTOM)
-    game.show_long_text("B per acabar", DialogLayout.BOTTOM)
-    while not (controller.B.is_pressed()):
-        if controller.up.is_pressed():
-            level2 += 1
-        elif controller.down.is_pressed():
-            level2 += 1
-        else:
-            game.show_long_text("Puja o baixa amb les flextes", DialogLayout.BOTTOM)
-        pause(2000)
-def init():
-    game.set_dryness_of_grass(randint(2, 4))
-    game.set_strength_of_wind(randint(2, 4))
-    game.set_health_of_trees(randint(4, 9))
-    hud.forest_hud_healthy(7)
-    hud.forest_hud_burned(2)
-    hud.danger_hud_label("Risc d'incendi")
-    hud.fire_hud_label("Focs:")
-    hud.forest_hud_label("Salut del bosc")
-    hud.fire_hud(True)
-    hud.danger_hud(True)
-    hud.forest_hud(True)
 
 def on_fire_destroyed(location2):
     scene.clear_particle_effects_at_location(location2)
@@ -99,7 +94,7 @@ info.on_life_zero(on_life_zero)
 
 def on_a_repeated():
     if settingLevel != 1:
-        sprites.spray(mySprite, assets.image("""
+        sprites.spray(firePlane, assets.image("""
             water
         """))
         music.play(music.create_sound_effect(WaveShape.SINE,
@@ -113,6 +108,41 @@ def on_a_repeated():
             music.PlaybackMode.IN_BACKGROUND)
 controller.A.on_event(ControllerButtonEvent.REPEATED, on_a_repeated)
 
+def chooseDifficulty():
+    global bforestA, bforestB, firePlane, settingLevel
+    tiles.set_tilemap(tilemap("""
+        level1
+    """))
+    music.play(music.string_playable("F G F A - F A G ", 130),
+        music.PlaybackMode.LOOPING_IN_BACKGROUND)
+    if settingLevel == 1 and difficulty == 0:
+        game.splash("Benvingut/da bomber/a!")
+        game.splash("Escull el bosc petit", "o el bosc gran")
+        game.show_long_text("Mou l'avió amb el cursor", DialogLayout.BOTTOM)
+        bforestA = sprites.create(assets.image("""
+            buttonA
+        """), SpriteKind.button)
+        bforestB = sprites.create(assets.image("""
+            buttonB
+        """), SpriteKind.button)
+        bforestA.set_position(30, 75)
+        bforestB.set_position(130, 70)
+        firePlane = sprites.create(assets.image("""
+                Fire Plane 2 Left
+            """),
+            SpriteKind.player)
+        firePlane.set_position(85, 70)
+        firePlane.set_bounce_on_wall(True)
+        firePlane.set_stay_in_screen(True)
+        controller.move_sprite(firePlane)
+    if difficulty > 0:
+        settingLevel = 0
+        sprites.destroy(bforestA)
+        sprites.destroy(bforestB)
+        sprites.destroy(firePlane)
+        music.stop_all_sounds()
+        start_game()
+
 def on_overlap_tile(sprite2, location3):
     sprite2.destroy(effects.ashes, 500)
     sprites.change_flame_strength_by(location3, -1)
@@ -122,72 +152,26 @@ scene.on_overlap_tile(SpriteKind.water,
     """),
     on_overlap_tile)
 
-def chooseDifficulty2():
-    global bforestA, bforestB, cursor2, settingLevel
-    tiles.set_tilemap(tilemap("""
-        level1
-    """))
+def on_on_overlap(sprite, otherSprite):
+    global difficulty
     if settingLevel == 1:
-        game.splash("Benvingut/da bomber/a!", "Escull el bosc A o el bosc B")
-        bforestA = sprites.create(assets.image("""
-            buttonA
-        """), SpriteKind.button)
-        bforestB = sprites.create(assets.image("""
-            buttonB
-        """), SpriteKind.button)
-        bforestA.set_position(25, 80)
-        bforestB.set_position(125, 80)
-        cursor2 = sprites.create(assets.image("""
-                Fire Plane 2 Left
-            """),
-            SpriteKind.cursor)
-        cursor2.set_position(70, 80)
-        controller.move_sprite(cursor2)
-    if difficulty > 0:
-        settingLevel = 0
-        sprites.destroy(bforestA)
-        sprites.destroy(bforestB)
-        sprites.destroy(cursor2)
-        start_game()
+        if otherSprite == bforestA:
+            difficulty = 1
+            chooseDifficulty()
+        elif otherSprite == bforestB:
+            difficulty = 2
+            chooseDifficulty()
+sprites.on_overlap(SpriteKind.player, SpriteKind.button, on_on_overlap)
 
-def start_game():
-    game.splash(difficulty)
-    if difficulty == 1:
-        tiles.set_tilemap(tilemap("""
-            level1
-        """))
-    else:
-        tiles.set_tilemap(tilemap("""
-            level2
-        """))
-    mySprite = sprites.create(assets.image("""
-            Fire Plane 2 Right
-        """),
-        SpriteKind.player)
-    controller.move_sprite(mySprite)
-    scene.camera_follow_sprite(mySprite)
-    game.show_long_text("Mou l'avió amb el cursor", DialogLayout.BOTTOM)
-    music.play(music.string_playable("B G B G B G B G ", 120),
-        music.PlaybackMode.LOOPING_IN_BACKGROUND)
-    game.show_long_text("Prem A per tirar aigua", DialogLayout.TOP)
-    init()
-    for index in range(randint(5, 20)):
-        sprites.create_spreading_fire(assets.tile("""
-                tree
-            """),
-            assets.tile("""
-                tree fire
-            """))
+bforestB: Sprite = None
+bforestA: Sprite = None
+difficulty = 0
+firePlane: Sprite = None
+settingLevel = 0
+level2 = 0
+settingLevel = 1
+chooseDifficulty()
 
 def on_on_update():
     sprites.random_spread()
 game.on_update(on_on_update)
-
-cursor2: Sprite = None
-level2 = 0
-bforestB: Sprite = None
-bforestA: Sprite = None
-mySprite: Sprite = None
-settingLevel = 1
-difficulty = 0
-chooseDifficulty2()
--- main.ts
+++ main.ts
@@ -1,95 +1,78 @@
 namespace SpriteKind {
     export const button = SpriteKind.create()
-    export const cursor = SpriteKind.create()
 }
 
 controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
     if (settingLevel != 1) {
-        mySprite.sayText("Som-hi!!", 500, true)
+        firePlane.sayText("Som-hi!!", 500, true)
     }
     
 })
+//  ################################################
+function init_config() {
+    if (difficulty == 1) {
+        game.splash("Bosc petit")
+        tiles.setTilemap(tilemap`
+            level1
+        `)
+        game.set_dryness_of_grass(randint(1, 3))
+        game.set_strength_of_wind(randint(1, 3))
+        game.set_health_of_trees(randint(5, 9))
+    } else if (difficulty == 2) {
+        game.splash("Bosc gran")
+        tiles.setTilemap(tilemap`
+            level2
+        `)
+        game.set_dryness_of_grass(randint(2, 4))
+        game.set_strength_of_wind(randint(2, 4))
+        game.set_health_of_trees(randint(4, 9))
+    }
+    
+    hud.danger_hud_label("Risc d'incendi")
+    hud.danger_hud(true)
+    hud.fire_hud_label("Focs:")
+    hud.fire_hud(true)
+    hud.forest_hud_label("Salut del bosc")
+    hud.forest_hud_healthy(7)
+    hud.forest_hud_burned(2)
+    hud.forest_hud(true)
+}
+
 sprites.on_fire_created(function on_fire_created(location: tiles.Location) {
     scene.createParticleEffectAtLocation(location, effects.fire)
     sprites.set_flame_strength(location, randint(15, 25))
     music.knock.play()
 })
-function chooseDifficulty1() {
-    
-    tiles.setTilemap(tilemap`
-        level1
-    `)
-    game.splash("Benvingut/da bomber/a!")
-    difficulty = game.askForNumber("Escull la dificultat del bosc (1 o 2)", 1)
-    while (difficulty != 1 && difficulty != 2) {
-        game.splash("Tria 1 o 2")
-        difficulty = game.askForNumber("Escull dificultat (1 o 2)", 1)
-    }
-}
-
 controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    animation.runImageAnimation(mySprite, assets.animation`
+    animation.runImageAnimation(firePlane, assets.animation`
             Fire Plane 2 Left Animation 1
         `, 300, true)
 })
-sprites.onOverlap(SpriteKind.cursor, SpriteKind.button, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+function start_game() {
     
-    if (settingLevel == 1) {
-        if (otherSprite == bforestA && controller.A.isPressed()) {
-            difficulty = 1
-            chooseDifficulty2()
-        }
-        
-        if (otherSprite == bforestB && controller.A.isPressed()) {
-            difficulty = 1
-            chooseDifficulty2()
-        }
-        
+    firePlane = sprites.create(assets.image`
+            Fire Plane 2 Right
+        `, SpriteKind.Player)
+    firePlane.setBounceOnWall(false)
+    controller.moveSprite(firePlane)
+    scene.cameraFollowSprite(firePlane)
+    init_config()
+    music.play(music.stringPlayable("B G B G B G B G ", 120), music.PlaybackMode.LoopingInBackground)
+    game.showLongText("Prem A per tirar aigua", DialogLayout.Top)
+    for (let index = 0; index < randint(4, 10); index++) {
+        sprites.create_spreading_fire(assets.tile`
+                tree
+            `, assets.tile`
+                tree fire
+            `)
     }
-    
-})
+}
+
 controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    animation.runImageAnimation(mySprite, assets.animation`
+    animation.runImageAnimation(firePlane, assets.animation`
             Fire Plane 2 Right Animation
         `, 300, true)
 })
-function choosePlane() {
-    game.splash("hola")
-}
-
-function chooseDifficulty0() {
-    
-    level2 = 0
-    game.showLongText("Escull la dificultat", DialogLayout.Full)
-    game.showLongText("Puja o baixa amb les flextes", DialogLayout.Bottom)
-    game.showLongText("B per acabar", DialogLayout.Bottom)
-    while (!controller.B.isPressed()) {
-        if (controller.up.isPressed()) {
-            level2 += 1
-        } else if (controller.down.isPressed()) {
-            level2 += 1
-        } else {
-            game.showLongText("Puja o baixa amb les flextes", DialogLayout.Bottom)
-        }
-        
-        pause(2000)
-    }
-}
-
-function init() {
-    game.set_dryness_of_grass(randint(2, 4))
-    game.set_strength_of_wind(randint(2, 4))
-    game.set_health_of_trees(randint(4, 9))
-    hud.forest_hud_healthy(7)
-    hud.forest_hud_burned(2)
-    hud.danger_hud_label("Risc d'incendi")
-    hud.fire_hud_label("Focs:")
-    hud.forest_hud_label("Salut del bosc")
-    hud.fire_hud(true)
-    hud.danger_hud(true)
-    hud.forest_hud(true)
-}
-
 sprites.on_fire_destroyed(function on_fire_destroyed(location2: tiles.Location) {
     scene.clearParticleEffectsAtLocation(location2)
     tiles.setTileAt(location2, assets.tile`
@@ -103,89 +86,79 @@ info.onLifeZero(function on_life_zero() {
 })
 controller.A.onEvent(ControllerButtonEvent.Repeated, function on_a_repeated() {
     if (settingLevel != 1) {
-        sprites.spray(mySprite, assets.image`
+        sprites.spray(firePlane, assets.image`
             water
         `)
         music.play(music.createSoundEffect(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
     }
     
 })
-scene.onOverlapTile(SpriteKind.Water, assets.tile`
-        tree fire
-    `, function on_overlap_tile(sprite2: Sprite, location3: tiles.Location) {
-    sprite2.destroy(effects.ashes, 500)
-    sprites.change_flame_strength_by(location3, -1)
-})
-function chooseDifficulty2() {
+function chooseDifficulty() {
     
     tiles.setTilemap(tilemap`
         level1
     `)
-    if (settingLevel == 1) {
-        game.splash("Benvingut/da bomber/a!", "Escull el bosc A o el bosc B")
+    music.play(music.stringPlayable("F G F A - F A G ", 130), music.PlaybackMode.LoopingInBackground)
+    if (settingLevel == 1 && difficulty == 0) {
+        game.splash("Benvingut/da bomber/a!")
+        game.splash("Escull el bosc petit", "o el bosc gran")
+        game.showLongText("Mou l'avió amb el cursor", DialogLayout.Bottom)
         bforestA = sprites.create(assets.image`
             buttonA
         `, SpriteKind.button)
         bforestB = sprites.create(assets.image`
             buttonB
         `, SpriteKind.button)
-        bforestA.setPosition(25, 80)
-        bforestB.setPosition(125, 80)
-        cursor2 = sprites.create(assets.image`
+        bforestA.setPosition(30, 75)
+        bforestB.setPosition(130, 70)
+        firePlane = sprites.create(assets.image`
                 Fire Plane 2 Left
-            `, SpriteKind.cursor)
-        cursor2.setPosition(70, 80)
-        controller.moveSprite(cursor2)
+            `, SpriteKind.Player)
+        firePlane.setPosition(85, 70)
+        firePlane.setBounceOnWall(true)
+        firePlane.setStayInScreen(true)
+        controller.moveSprite(firePlane)
     }
     
     if (difficulty > 0) {
         settingLevel = 0
         sprites.destroy(bforestA)
         sprites.destroy(bforestB)
-        sprites.destroy(cursor2)
+        sprites.destroy(firePlane)
+        music.stopAllSounds()
         start_game()
     }
     
 }
 
-function start_game() {
-    game.splash(difficulty)
-    if (difficulty == 1) {
-        tiles.setTilemap(tilemap`
-            level1
-        `)
-    } else {
-        tiles.setTilemap(tilemap`
-            level2
-        `)
+scene.onOverlapTile(SpriteKind.Water, assets.tile`
+        tree fire
+    `, function on_overlap_tile(sprite2: Sprite, location3: tiles.Location) {
+    sprite2.destroy(effects.ashes, 500)
+    sprites.change_flame_strength_by(location3, -1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.button, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    
+    if (settingLevel == 1) {
+        if (otherSprite == bforestA) {
+            difficulty = 1
+            chooseDifficulty()
+        } else if (otherSprite == bforestB) {
+            difficulty = 2
+            chooseDifficulty()
+        }
+        
     }
     
-    let mySprite = sprites.create(assets.image`
-            Fire Plane 2 Right
-        `, SpriteKind.Player)
-    controller.moveSprite(mySprite)
-    scene.cameraFollowSprite(mySprite)
-    game.showLongText("Mou l'avió amb el cursor", DialogLayout.Bottom)
-    music.play(music.stringPlayable("B G B G B G B G ", 120), music.PlaybackMode.LoopingInBackground)
-    game.showLongText("Prem A per tirar aigua", DialogLayout.Top)
-    init()
-    for (let index = 0; index < randint(5, 20); index++) {
-        sprites.create_spreading_fire(assets.tile`
-                tree
-            `, assets.tile`
-                tree fire
-            `)
-    }
-}
-
+})
+let bforestB : Sprite = null
+let bforestA : Sprite = null
+let difficulty = 0
+let firePlane : Sprite = null
+let settingLevel = 0
+let level2 = 0
+settingLevel = 1
+chooseDifficulty()
 game.onUpdate(function on_on_update() {
     sprites.random_spread()
 })
-let cursor2 : Sprite = null
-let level2 = 0
-let bforestB : Sprite = null
-let bforestA : Sprite = null
-let mySprite : Sprite = null
-let settingLevel = 1
-let difficulty = 0
-chooseDifficulty2()
